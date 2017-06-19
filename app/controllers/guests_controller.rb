aa7c4fc869a347c5b1dd4fcba18522fff310014c
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-06-19 11:03:25 -0400

    Done.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,25 @@
 class GuestsController < ApplicationController
+before_action :set_guest, only: [:show, :appearance]
+
 
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def appearance
+    @appearance = Appearance.create(episode_id:params[:appearance][:episode][:number], guest:set_guest, rating: params[:appearance][:rating])
+    flash[:notice] = "Appearance added in episode #{params[:appearance][:episode][:number]}"
+    redirect_to guest_path(set_guest)
+  end
+
+  private
+
+  def set_guest
+    @guest = Guest.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  resources :guests do
+    member do
+     post 'appearance'
+    end
+  end
+
+  resources :episodes, only: [:index, :show]
+
+  get '/login', to: 'sessions#new', as: 'login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  resources :sessions
+
+  get '/signup', to: 'users#new', as: 'signup'
+  resources :users
+
 end
